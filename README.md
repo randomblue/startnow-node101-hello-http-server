efc66cfc9059b4b6a52bb86d756901d99d2f0539
randomblue <jpmedallon@gmail.com>
2018-02-26 18:46:32 -0800

    Project to create a HTTP Server - Hello World

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello World - HTTP Server
+
+## Creating a HTTP server
+
+```
+git clone https://github.com/randomblue/startnow-node101-hello-http-server.git
+cd startnow-node101-hello-http-server
+npm install
+npm install nightmare
+npm install chai
+npm install axios
+npm start
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+// write your code here
+var http = require('http');
+http.createServer(function (request, response) {
+ response.writeHead(200, {'Content-Type':'text/plain' });
+ response.write('Hello World');
+ response.end();
+ 
+}).listen(8080, 'localhost');
+console.log('Server is listening on http://localhost:8080 Received GET request for: /');
